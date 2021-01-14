3e74dc46b43353717ab82d4a9ae475c85455a655
AntonioHernandezO <hxckredx@gmail.com>
2021-01-14 03:21:20 -0600

    Service terminado

--- public_html/php/services_h.php
+++ public_html/php/services_h.php
@@ -4,6 +4,10 @@ require_once 'db.php';
 
 $id_service=$_POST['id_ser'];
 $con_service=$_POST['con_ser'];
+$tipo=$_POST['tipo'];
+
+
+if ($tipo==1){
 
 if( $id_service != "" && $con_service != ""){
 
@@ -25,7 +29,44 @@ if( $id_service != "" && $con_service != ""){
    }
 }
 
+}else if($tipo==2){
+    if( $id_service != "" && $con_service != ""){
 
+        try {
+        
+         $sql = $conectar->prepare("UPDATE servicio  SET concept = '".$con_service."'  WHERE id_ser=".$id_service."");
+         
+         $sql->execute();
+         
+         $sql=null; 
+                   
+     
+         header("Location: ../view/services_h.php");
+        } catch (\Throwable $th) {
+            echo"POR ALGUNA RAZON NO SE PUDO ACTUALIZAR EL REGISTRO, INTENTELO DE NUEVO";
+        }
+     }
 
-?>
+}elseif($tipo==3){
 
+    if( $id_service != ""){
+
+        try {
+        
+         $sql = $conectar->prepare("DELETE FROM servicio WHERE id_ser=".$id_service);
+         
+         $sql->execute();
+         
+         $sql=null; 
+                   
+     
+         header("Location: ../view/services_h.php");
+        } catch (\Throwable $th) {
+            echo"POR ALGUNA RAZON NO SE PUDO ELMINAR EL REGISTRO, INTENTELO DE NUEVO";
+        }
+     }
+}
+
+else{
+    echo"ERROR 404"; 
+}
