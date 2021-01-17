77372b8f857346e55ffed37bbb8b8ef6f8f25ee0
AntonioHernandezO <hxckredx@gmail.com>
2021-01-16 23:45:50 -0600

    S/mamadas

--- public_html/php/faculta_h.php
+++ public_html/php/faculta_h.php
@@ -22,13 +22,9 @@ if( $id_service != "" && $num_reg != ""&& $name != ""&& $appat != ""&& $apmat !=
     $sql = $conectar->prepare("INSERT INTO faculta VALUES ('".$num_reg."','".$id_service."','".$name."','".$appat."','".$apmat."','".$dir."','".$tel."','".$pwd."'   )     ");
     $sql->execute();
     
-    $sql=null; //OJO, ES EXTREMADAMENTE IMPORTANTE IGUALAR LA CONEXION A NULL, PUES AVECES LOS HACKERS RUSOS ESTAN AL ACECHO DE LOS 
-              // PROYECTITOS DE LOS ESTUDIANTES DE LA HONORABLE FACULTAD DE ESTUDIOS SUPERIORES CUAUTITLAN.
+    $sql=null; 
 
-              /**
-               OJO, NO ES NECESARIO CERRAR LA CONEXION PUES USAMOS PDO, ANTES DE QUE ME LA EMPIECEN A SUCCIONAR
-               https://es.stackoverflow.com/questions/50083/es-necesario-cerrar-una-conexi%C3%B3n-con-pdo-luego-de-ejecutar-una-sentencia#:~:text=La%20conexi%C3%B3n%20permanecer%C3%A1%20activa%20durante,variable%20que%20contiene%20el%20objeto.
-               **/
+            
     header("Location: ../view/faculta_h.php");
    } catch (\Throwable $th) {
        echo"POR ALGUNA RAZON NO SE PUDO INSERTAR EL REGISTRO, INTENTELO DE NUEVO";
